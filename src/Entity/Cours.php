4392bf52e5536d7d17076446c6f41ba34331a037
Mokrane Rachedi <mocco1995ra@gmail.com>
2021-05-05 18:32:53 +0100

    Affichage du nombre de cours publiés dans chaque classe de cours

--- src/Entity/Cours.php
+++ src/Entity/Cours.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Entity\Traits\Timestampable;
 use App\Repository\CoursRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CoursRepository::class)
