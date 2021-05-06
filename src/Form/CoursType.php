0036c90219f954a23a70a0a4d7cfe0f06996a9a3
Mokrane Rachedi <mocco1995ra@gmail.com>
2021-05-06 01:28:46 +0100

    Ajout du champ d'upload de fichier dans CoursType

--- src/Form/CoursType.php
+++ src/Form/CoursType.php
@@ -8,12 +8,19 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class CoursType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('fichierCours', VichFileType::class, [
+                'label' => 'Fichier(PDF, DOC et DOCX)',
+                'required' => false,
+                'allow_delete' => false,
+                'download_uri' => false
+            ])
             ->add('titre', TextType::class, [
                 'label' => 'Titre *',
                 'attr' => [
