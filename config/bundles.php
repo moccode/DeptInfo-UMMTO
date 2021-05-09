1867eb2388d6cc6d6290e137bc95df87a971b5c8
Mokrane Rachedi <mocco1995ra@gmail.com>
2021-05-09 18:42:18 +0100

    Ajout de flashy Notifier

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => ['all' => true],
 ];
