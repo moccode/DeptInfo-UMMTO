bd603ee71a81d707ad6b242f393d0483b141a330
Mokrane Rachedi <mocco1995ra@gmail.com>
2021-05-21 16:07:34 +0100

    Ajout de la fonctionnalité réinitialiser le mot de passe

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
